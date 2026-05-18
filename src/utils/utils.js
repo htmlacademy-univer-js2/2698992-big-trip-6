ffd6f492b8bd03a7371706d523f3574bde99ca58
yadirfd <formygithubproject@gmail.com>
2026-05-18 08:28:09 +0500

    fix eslint (2)

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -30,7 +30,7 @@ export const calculateDuration = (dateFrom, dateTo) => {
 };
 
 export const sortPointByDay = (pointA, pointB) =>
-  new Date(pointA.dateFrom) - new Date(pointB.dateFrom)
+  new Date(pointA.dateFrom) - new Date(pointB.dateFrom);
 
 export const sortPointByTime = (pointA, pointB) => {
   const timeA = new Date(pointA.dateTo) - new Date(pointA.dateFrom);
